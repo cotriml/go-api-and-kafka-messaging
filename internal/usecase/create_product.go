64718c5cc605f3aea265446b38ad3e2a671b90b3
cotriml <lucascotrim3@hotmail.com>
2023-04-24 15:53:21 -0300

    Added the connectors to the app + docker

--- internal/usecase/create_product.go
+++ internal/usecase/create_product.go
@@ -3,8 +3,8 @@ package usecase
 import "github.com/cotriml/go-api-and-kafka-messaging/internal/entity"
 
 type CreateProductInputDto struct {
-	Name  string
-	Price float64
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 type CreateProductOutputDto struct {
